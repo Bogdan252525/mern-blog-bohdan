44cc27411f7f9f6939bd4fb3963451f60a6ca0ba
Bogdan252525 <bogdan2516@gmail.com>
2024-07-17 15:12:00 +0300

    add file json to back

--- mern-blog-backend/index.js
+++ mern-blog-backend/index.js
@@ -33,7 +33,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+	{
+		origin: ["https://mern-blog-bohdan-1whq.vercel.app"],
+		methods: ["POST", "GET"],
+		credentials: true
+	}
+));
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
