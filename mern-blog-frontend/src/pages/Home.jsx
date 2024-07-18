b3c54857695b0d04d3994dc3114b1bf4439d270a
Bogdan252525 <bogdan2516@gmail.com>
2024-07-18 03:32:35 +0300

    setup frontend

--- mern-blog-frontend/src/pages/Home.jsx
+++ mern-blog-frontend/src/pages/Home.jsx
@@ -37,7 +37,7 @@ export const Home = () => {
               <Post
                 id={obj._id}
                 title={obj.title}
-                imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ''}
+                imageUrl={obj.imageUrl ? `https://mern-blog-backend-zk2s.onrender.com${obj.imageUrl}` : ''}
                 user={obj.user}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
